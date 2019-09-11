8e4dc91b87adbcc420a1e95897c5ab27e4c629f4
wdnsds <rifqi.majid29@gmail.com>
2019-09-11 16:40:21 +0700

    fix carousel

--- resources/views/front-end/marketplace/prospective.blade.php
+++ resources/views/front-end/marketplace/prospective.blade.php
@@ -1,6 +1,197 @@
 @extends('front-end.master.newest-master')
+@section('css')
+    <style>
+
+        .carousel{
+            width:100%;
+            max-width:1280px;
+            max-height:600px;
+            font-family: 'Open Sans', sans-serif;
+        }
+        .carousel .item {
+            position: relative;
+        }
+        .carousel .item .imageContainer {
+            display:block;
+            width:100%;
+            position:relative;
+            max-height:600px;
+        }
+
+        .carousel .item .imageContainer:before{
+            content:"";
+            position: absolute;
+            top:0px;
+            left:0px;
+            color:red;
+            width:100%;
+            height:100%;
+            background: -moz-linear-gradient(top, rgba(0,0,0,0) 0%, rgba(0,0,0,0.75) 90%, rgba(0,0,0,0.75) 100%); /* FF3.6+ */
+            background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(0,0,0,0)), color-stop(90%,rgba(0,0,0,0.75)), color-stop(100%,rgba(0,0,0,0.75))); /* Chrome,Safari4+ */
+            background: -webkit-linear-gradient(top, rgba(0,0,0,0) 0%,rgba(0,0,0,0.75) 90%,rgba(0,0,0,0.75) 100%); /* Chrome10+,Safari5.1+ */
+            background: -o-linear-gradient(top, rgba(0,0,0,0) 0%,rgba(0,0,0,0.75) 90%,rgba(0,0,0,0.75) 100%); /* Opera 11.10+ */
+            background: -ms-linear-gradient(top, rgba(0,0,0,0) 0%,rgba(0,0,0,0.75) 90%,rgba(0,0,0,0.75) 100%); /* IE10+ */
+            background: linear-gradient(to bottom, rgba(0,0,0,0) 0%,rgba(0,0,0,0.75) 90%,rgba(0,0,0,0.75) 100%); /* W3C */
+            filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#00000000', endColorstr='#bf000000',GradientType=0 ); /* IE6-9 */
+        }
+
+        .carousel .item img {
+            display:block;
+            width:100%;
+        }
+
+        .carousel .item .overlay {
+            z-index: 999;
+            position:absolute;
+            bottom:0px;
+            left:0px;
+            right:0px;
+            color:#fff;
+            padding: 30px 0px 70px 0px;
+
+        }
+
+        .carousel .item .overlay h3{
+            text-align: center;
+            font-size: 40px;
+            font-weight: 300;
+            padding-bottom: 20px;
+            width: 70%;
+            margin: 0 auto;
+        }
+        .carousel .item .overlay p{
+            text-align: center;
+            width: 60%;
+            margin: 0 auto;
+            font-size: 16px;
+            font-weight: 300;
+            line-height: 29px;
+            color: rgba(255, 255, 255, 0.7);
+        }
 
 
+        .carousel .slick-dots{
+            position:absolute;
+            bottom:10px;
+            z-index: 999;
+        }
+
+        .slick-dots li button:before
+        {
+            color: #fff;
+        }
+
+        .slick-dots li.slick-active button:before
+        {
+            color: #fff;
+        }
+
+        @media only screen and (min-width: 901px) and (max-width: 1100px) {
+            .carousel {
+                max-height:600px;
+            }
+            .carousel .item .imageContainer{
+                max-height:600px;
+            }
+
+            .carousel .item .overlay{
+                padding-bottom: 55px;
+            }
+            .carousel .item .overlay h3{
+                width: 80%;
+                font-size: 34px;
+                line-height: 40px;
+                padding-bottom:10px;
+            }
+            .carousel .item .overlay p{
+                width: 65%;
+                font-size:15px;
+                line-height: 25px;
+            }
+        }
+
+        @media only screen and (min-width: 651px) and (max-width: 900px) {
+            .carousel .item .overlay{
+                padding-bottom: 50px;
+            }
+            .carousel .item .overlay h3{
+                width: 80%;
+                font-size: 26px;
+                line-height: 30px;
+                padding-bottom:10px;
+            }
+            .carousel .item .overlay p{
+                width: 70%;
+                font-size:14px;
+                line-height: 20px;
+            }
+        }
+
+        @media only screen and (max-width: 650px) {
+            .carousel .item .overlay{
+                position: relative;
+                background: inherit;
+                color:#000;
+                padding: 25px 20px 40px 20px;
+            }
+            .carousel .item .overlay h3{
+                width: 100%;
+                text-align: left;
+                color: rgba(0,0,0,0.9);
+                font-size: 27px;
+                font-weight: 400;
+                padding-bottom:10px;
+            }
+            .carousel .item .overlay p{
+                width: 100%;
+                text-align: left;
+                color: rgba(102, 102, 102, 0.9);
+                font-size:17px;
+            }
+
+            .carousel .slick-dots{
+                bottom: -3px;
+            }
+            .slick-dots li button:before
+            {
+                color: #000;
+            }
+
+            .slick-dots li.slick-active button:before
+            {
+                color: #000;
+            }
+
+        }
+
+        @media only screen and (max-width: 550px) {
+            .carousel .item .overlay{
+                padding: 15px 15px 40px 15px;
+            }
+            .carousel .item .overlay h3{
+                font-size: 24px;
+                line-height: 29px;
+            }
+            .carousel .item .overlay p{
+                font-size:13px;
+                line-height: 21px;
+            }
+        }
+
+        @media only screen and (max-width: 450px) {
+            .carousel .item .overlay h3{
+                font-size: 20px;
+                line-height: 25px;
+            }
+            .carousel .item .overlay p{
+                font-size:13px;
+                line-height: 21px;
+            }
+        }
+
+        @import url('https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,600,700,300,800');
+    </style>
+@endsection
 @section('header')
     <div id="headerEn">
         <header class="header-home header-home--color">
@@ -61,21 +252,24 @@
                                 </div>
                             </div>
                             <div class="col-6 about-app__img about-app__img--left">
-                                <div class="about-app__img-wrap">
 
-                                    @isset($proyek->fotos)
-                                        @php
-                                            $images = json_decode($proyek->fotos)
-                                        @endphp
+                                @isset($proyek->fotos)
+                                    @php
+                                        $images = json_decode($proyek->fotos)
+                                    @endphp
+                                    <div class="carousel">
+                                        @foreach((array) $images as $image)
+                                            <div class="item">
+                                                <div class="about-app__img-wrap">
+                                                    <img src="{{Voyager::image($image)}}" alt="">
+                                                </div>
+                                            </div>
+                                        @endforeach
+                                    </div>
 
-
-                                        <img src="{{Voyager::image($images[0])}}" alt="">
-
-                                    @else
-                                        <img alt="" src="{{'storage/'.Voyager::setting('site.not_found')}}">
-                                    @endisset
-
-                                </div>
+                                @else
+                                    <img alt="" src="{{'storage/'.Voyager::setting('site.not_found')}}">
+                                @endisset
                             </div>
                         </div>
 
@@ -234,21 +428,24 @@
                                 </div>
                             </div>
                             <div class="col-6 about-app__img about-app__img--left">
-                                <div class="about-app__img-wrap">
 
-                                    @isset($proyek->fotos)
-                                        @php
-                                            $images = json_decode($proyek->fotos)
-                                        @endphp
+                                @isset($proyek->fotos)
+                                    @php
+                                        $images = json_decode($proyek->fotos)
+                                    @endphp
+                                    <div class="carousel">
+                                        @foreach((array) $images as $image)
+                                            <div class="item">
+                                                <div class="about-app__img-wrap">
+                                                    <img src="{{Voyager::image($image)}}" alt="">
+                                                </div>
+                                            </div>
+                                        @endforeach
+                                    </div>
 
-
-                                        <img src="{{Voyager::image($images[0])}}" alt="">
-
-                                    @else
-                                        <img alt="" src="{{'storage/'.Voyager::setting('site.not_found')}}">
-                                    @endisset
-
-                                </div>
+                                @else
+                                    <img alt="" src="{{'storage/'.Voyager::setting('site.not_found')}}">
+                                @endisset
                             </div>
                         </div>
 
@@ -390,261 +587,22 @@
 
 
 @section('js')
-    {{--@if($videos->isNotEmpty())
-        <script>
-            var tag = document.createElement('script');
-            tag.src = "https://www.youtube.com/iframe_api";
-            var firstScriptTag = document.getElementsByTagName('script')[0];
-            firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-            var player;
-            var idnya = document.getElementById('idnya');
-
-            //console.log({{$id}});
-
-            function onYouTubeIframeAPIReady() {
-                player = new YT.Player('player', {
-                    height: '315',
-                    width: '560',
-                    videoId: '{{$id}}',
-                    events: {
-                        'onReady': function() {
-                            $(".video-thumb").click(function() {
-                                var $this = $(this);
-                                if (!$this.hasClass("active")) {
-                                    player.loadVideoById($this.attr("data-video"));
-                                    $this.addClass("active").siblings().removeClass("active");
-                                }
-                            });
-                        }
-                    }
-                });
-            }
-        </script>
-    @endif--}}
     <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
     <script src="https://code.highcharts.com/highcharts.js"></script>
     <script src="https://code.highcharts.com/modules/exporting.js"></script>
-    {{--<script>
-        // Data gathered from http://populationpyramid.net/germany/2015/
-
-        // Age categories
-        var categories = [];
-        var pria = [];
-        var wanita = [];
-
-        var test = {!! $tes !!};
-
-        var i;
-        for(i=0; i<test.length; i++){
-            categories.push(test[i].kecamatan_id);
-            pria.push(-Math.abs(test[i].lelaki));
-            wanita.push(test[i].perempuan);
-        }
-        var sumber = test[0].sumber_data;
-
-        Highcharts.chart('penduduk', {
-            chart: {
-                type: 'bar'
-            },
-            title: {
-                text: 'Kepadatan Penduduk '+test[0].kab_kota_id
-            },
-            subtitle: {
-                text: 'Source: '+sumber
-            },
-            xAxis: [{
-                categories: categories,
-                reversed: false,
-                labels: {
-                    step: 1
-                }
-            }, { // mirror axis on right side
-                opposite: true,
-                reversed: false,
-                categories: categories,
-                linkedTo: 0,
-                labels: {
-                    step: 1
-                }
-            }],
-            yAxis: {
-                title: {
-                    text: null
-                },
-                labels: {
-                    formatter: function () {
-                        return Math.abs(this.value);
-                    }
-                }
-            },
-
-            plotOptions: {
-                series: {
-                    stacking: 'normal'
-                }
-            },
-
-            tooltip: {
-                formatter: function () {
-                    return '<b>' + this.series.name + ', kecamatan ' + this.point.category + '</b><br/>' +
-                        'Population: ' + Highcharts.numberFormat(Math.abs(this.point.y), 0);
-                }
-            },
-            credits: {
-                enabled: false
-            },
-            series: [{
-                name: 'Male',
-                data: pria,
-            }, {
-                name: 'Female',
-                data: wanita
-            }]
-        });
-
-    </script>
-    <script>
-        /*JS FOR SCROLLING THE ROW OF THUMBNAILS*/
-        $(document).ready(function () {
-            $('.vid-item').each(function(index){
-                $(this).on('click', function(){
-                    var current_index = index+1;
-                    $('.vid-item .thumb').removeClass('active');
-                    $('.vid-item:nth-child('+current_index+') .thumb').addClass('active');
-                });
-            });
-        });
-    </script>
-    <script>
-        function moreLess(initiallyVisibleCharacters) {
-            var visibleCharacters = initiallyVisibleCharacters;
-            var paragraph = $(".text");
-
-
-
-
-            paragraph.each(function() {
-                var text = $(this).text();
-                var wholeText = text.slice(0, visibleCharacters) + "<span class='ellipsis'>... </span><a href='#' class='more'>MORE<i class='fa fa-arrow-circle-o-down' aria-hidden='true'></i></a>" + "<span style='display:none'>" + text.slice(visibleCharacters, text.length) + "<a href='#' class='less'> LESS<i class='fa fa-arrow-circle-o-up' aria-hidden='true'></i></a></span>"
-
-                if (text.length < visibleCharacters) {
-                    return
-                } else {
-                    $(this).html(wholeText)
-                }
-            });
-            $(".more").click(function(e) {
-                e.preventDefault();
-                $(this).hide().prev().hide();
-                $(this).next().show();
-            });
-            $(".less").click(function(e) {
-                e.preventDefault();
-                $(this).parent().hide().prev().show().prev().show();
-            });
-        };
-
-        moreLess(300);
-    </script>
-    <script>
-        var tahun = [];
-        var umr = [];
-        var umrs = {!! $umrs !!};
-        console.log(umrs);
-        var i;
-
-        for(i=0; i<umrs.length; i++){
-            tahun.push(umrs[i].tahun);
-            umr.push(Math.abs(umrs[i].nilai_umr));
-        }
-        console.log(umr)
-        var sumber = umrs[0].sumber_data;
-        //console.log(umr);
-        Highcharts.chart('umr', {
-            chart: {
-                type: 'column'
-            },
-            title: {
-                text: 'UMR'
-            },
-            subtitle: {
-                text: 'Source: '+sumber
-            },
-            xAxis: {
-                categories: tahun,
-                crosshair: true
-            },
-            yAxis: {
-                min: 0,
-                title: {
-                    text: 'Juta (Rp)'
-                }
-            },
-            tooltip: {
-                headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-                pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: Rp </td>' +
-                    '<td style="padding:0"><b>{point.y} </b></td></tr>',
-                footerFormat: '</table>',
-                shared: true,
-                useHTML: true
-            },
-            plotOptions: {
-                column: {
-                    pointPadding: 0.2,
-                    borderWidth: 0
-                }
-            },
-            credits: {
-                enabled: false
-            },
-            series: [{
-                name: 'UMR',
-                data: umr
-
-            }]
-        });
-    </script>
-    <script>
-        $(document).ready(function() {
-            // Configure/customize these variables.
-            var showChar = 100;  // How many characters are shown by default
-            var ellipsestext = "...";
-            var moretext = "Show more >";
-            var lesstext = "Show less";
-
-
-            $('.more').each(function() {
-                var content = $(this).html();
-
-
-                if(content.length > showChar) {
-                    var c = content.substr(0, showChar);
-                    var h = content.substr(showChar, content.length - showChar);
-                    console.log(h);
-
-                    var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
-
-                    $(this).html(html);
-                }
-
-            });
-
-            $(".morelink").click(function(){
-                if($(this).hasClass("less")) {
-                    $(this).removeClass("less");
-                    $(this).html(moretext);
-                } else {
-                    $(this).addClass("less");
-                    $(this).html(lesstext);
-                }
-                $(this).parent().prev().toggle();
-                $(this).prev().toggle();
-                return false;
-            });
-        });
-    </script>--}}
     <script src=https://cdnjs.cloudflare.com/ajax/libs/echarts/4.0.2/echarts-en.min.js charset=utf-8></script>
-    <script src="{{asset('js/front-end/carousel.js')}}"></script>
-    {{--{!! ($chart->script()) !!}--}}
+    <script src="{{asset('js/front-end/vendor/slick.min.js')}}"></script>
+    <script>
+        $('.carousel').slick({
+            dots: true,
+            infinite: true,
+            speed: 300,
+            slidesToShow: 1,
+            centerMode: false,
+            variableWidth: false,
+            autoplay: true,
+            arrows: false
+        });
+    </script>
 @endsection
 
